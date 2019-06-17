4f92000d13443f6ecbe5fdb19e4de644c12156ba
Gabriel Poissant <poissantgabriel@gmail.com>
2019-06-17 13:46:01 -0400

    testing

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,9 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = ENV["API_KEY"]
-  config.secret = ENV["API_SECRET"]
+  config.api_key = "49b015b80ac514aa573b37c00979a009"
+  config.secret = "edd1675914d6f6af512b40bc0ded0885"
+  # config.api_key = ENV["API_KEY"]
+  # config.secret = ENV["API_SECRET"]
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
